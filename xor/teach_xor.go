ebc2eb00961ca749e9278a16a857da7da7ecb637
Colin McRae <colinm2718@gmail.com>
2024-09-13 12:41:58 -0400

    first commit

---
+++ xor/teach_xor.go
@@ -0,0 +1,20 @@
+package xor
+
+var network [][]float64
+var trainingData map[float64]float64
+
+func getTrainingData(count int) {
+	trainingData = make(map[float64]float64, count)
+	for i := 0; i < count; i++ {
+
+		trainingData[]
+	}
+}
+
+func train() {
+
+}
+
+func inputOutputToKeyValue(x1, x2, y bool) {
+	
+}
